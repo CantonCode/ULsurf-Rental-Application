8e6d655c980730ad2a5d57d961f989ce39789fde
Conor Canton <conorstreete@gmail.com>
2020-03-26 12:45:40 +0000

    fix

--- clubApp/app/src/main/java/com/example/clubapp/homeActivity.java
+++ clubApp/app/src/main/java/com/example/clubapp/homeActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
