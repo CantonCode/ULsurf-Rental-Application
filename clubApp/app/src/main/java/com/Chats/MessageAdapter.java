a82dd6f561100074af1687602d76ac2cc11a1971
Conor Canton <conorstreete@gmail.com>
2020-04-13 15:50:17 +0100

    fixed chat app

--- clubApp/app/src/main/java/com/Chats/MessageAdapter.java
+++ clubApp/app/src/main/java/com/Chats/MessageAdapter.java
@@ -13,79 +13,82 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.Login.User;
 import com.example.clubapp.R;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.ocpsoft.prettytime.PrettyTime;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MessageAdapter extends FirestoreRecyclerAdapter<Message,MessageAdapter.MessageHolder> {
 
 
-    private static final int SENT = 0;
-    private static final int RECEIVED = 1;
+    private static final int LEFT = 0;
+    private static final int RIGHT = 1;
+
+    PrettyTime p = new PrettyTime();
 
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseUser user = mAuth.getCurrentUser();
+    String userName;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public MessageAdapter( FirestoreRecyclerOptions<Message> options){
         super(options);
     }
 
 
-    protected void onBindViewHolder(final MessageHolder holder, int position,Message model) {
+    protected void onBindViewHolder(final MessageHolder holder, int position,final Message model) {
 //        message = model;
 
+        DocumentReference docRef = db.collection("users").document(model.getSender());
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                holder.message.setText(model.getMessage());
+                holder.sender.setText(documentSnapshot.get("userName").toString());
+                holder.timestamp.setText(p.format(model.getTime()));
+            }
+        });
 
-        Log.d("MessageAdapter", model.getMessage());
-
-        if(user.getUid().equals(model.getSender())){
-            holder.mContentForReceiver.setTextColor(Color.WHITE);
-            holder.mContentForReceiver.setText(model.getMessage());
-            holder.mContentForSender.setVisibility(View.GONE);
-        }else{
-            holder.mContentForSender.setTextColor(Color.WHITE);
-            holder.mContentForSender.setText(model.getMessage());
-            holder.mContentForReceiver.setVisibility(View.GONE);
-        }
     }
 
 
     @NonNull
     @Override
     public MessageHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
 
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_card,parent,false);
-        final MessageHolder holder = new MessageHolder(v);
-//        if(viewType == SENT)
-//        {
-//            view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_left,parent,false);
-//        }
-//        if(viewType == RECEIVED)
-//        {
-//            view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_right,parent,false);
-//        }
+        if(viewType == RIGHT)
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_right, parent, false);
+        else
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_left, parent, false);
 
-        return holder;
-
+        return new MessageHolder(view);
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if(getItem(position).getSender().equals(user.getUid()))
+            return RIGHT;
+        else
+            return LEFT;
+    }
 
-
-
-//    @Override
-//    public int getItemViewType(int position) {
-//        if (message.getSender().equals(userID))
-//            return SENT;
-//        else
-//            return RECEIVED;
-//    }
 
     class MessageHolder extends RecyclerView.ViewHolder {
 
 
         //Variables for my chat lists...
-        TextView mContentForSender , mContentForReceiver;
+        TextView sender , message, timestamp;
 
         CardView display_chat;
 
@@ -94,8 +97,9 @@ public class MessageAdapter extends FirestoreRecyclerAdapter<Message,MessageAdap
             super(itemView);
 
             //My variable initialization
-            mContentForSender = itemView.findViewById(R.id.show_message);
-            mContentForReceiver = itemView.findViewById(R.id.show_message1);
+            sender = itemView.findViewById(R.id.sendername);
+            message = itemView.findViewById(R.id.textmessage);
+            timestamp = itemView.findViewById(R.id.timestamp);
             //display_chat = itemView.findViewById(R.id.user_chatBox);
 
 //            mContent =itemView.findViewById(R.id.list_studentNumber);
