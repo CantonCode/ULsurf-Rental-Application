60cc3cd9733af13a2c15ccf25ca9bf4d53c52e12
Conor Canton <conorstreete@gmail.com>
2020-04-24 11:56:32 +0100

    started adding layout for managing boards

---
+++ clubApp/app/src/main/java/com/Admin/manage_boards.java
@@ -0,0 +1,16 @@
+package com.Admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clubapp.R;
+
+public class manage_boards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_boards);
+    }
+}
